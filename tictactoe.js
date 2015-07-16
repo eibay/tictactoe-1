966d8ce85676c24fce12ae3687dc5f89ed8a3915
Jenny Tran <tran.j@live.com>
2015-07-16 23:03:20 +1000

    finalised css

--- tictactoe.js
+++ tictactoe.js
@@ -3,10 +3,12 @@ console.log('This is Tic Tac Toe')
 //GAME
 var game = {
   puzzle: [' ',' ',' ',' ', ' ', ' ', ' ', ' ', ' '],
-  score: '',
+  scoreOne: '',
+  scoreTwo: '',
   counter: 0,
   indexArrayX: [],
   indexArrayO: [],
+  rounds: 0,
 
 
   setPlay: function() {
@@ -59,7 +61,7 @@ var game = {
   },
 
   init: function() {
-    game.puzzle()
+    game.puzzle = [' ',' ',' ',' ', ' ', ' ', ' ', ' ', ' ']
   }
 }
 
@@ -69,6 +71,8 @@ var game = {
 //LOADING FUNCTIONS
 window.onload = function() {
 
+  // $('#reset').on('click', game.init);
+
   _.each(game.puzzle, function(marker, index) {
     // add div box
     var newDivBox = $('<div>').attr('id','span-' + index).html(marker);
@@ -77,7 +81,6 @@ window.onload = function() {
   });
 
   $('#game').on('click', 'div', function(event) {
-
     var index = parseInt(this.id.split('-')[1]); // getting the index number of the div being clicked to apply it back to the array
     console.log('index', index);
     // game.puzzle[index] = 'x';
@@ -86,6 +89,7 @@ window.onload = function() {
     var markerX = 'x';
     var markerO = 'o';
     game.counter++
+    game.rounds++
 
 //MARKING X IN ARRAY, MARKING X ON THE BOARD, AND ALSO INPUTTING INDEX INTO NEW ARRAY FOR COMPARISON WITH WINNER
       if (game.counter % 2 === 1) {
@@ -100,8 +104,14 @@ window.onload = function() {
         console.log('o appears at: ' + index);
       }
       console.log('win? ' + game.checkWin());
-      if (game.checkWin() === true) {
-        alert(game.counter[index] + 'HAVE WON!')
+      if (game.checkWin() === true && game.counter % 2 === 1) {
+        alert('X IS THE WINNER!')
+        $('#xScore').html(game.rounds);
+      } else if (game.checkWin() === true && game.counter % 2 === 0) {
+        alert('O IS THE WINNER!')
+        $('#oScore').html(game.rounds);
+      } else if (game.counter === 9) {
+        alert ("It's a tie. Play again!")
       }
   });
 
