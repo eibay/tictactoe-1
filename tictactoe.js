462ad2769e16094f9d22020b193bb7bc85f42dfe
Jenny Tran <tran.j@live.com>
2015-07-16 13:01:02 +1000

    made arrays for x and o

--- tictactoe.js
+++ tictactoe.js
@@ -3,41 +3,41 @@ console.log('This is Tic Tac Toe')
 //GAME
 var game = {
   puzzle: [' ',' ',' ',' ', ' ', ' ', ' ', ' ', ' '],
-  players: [1, 2],
-  nextTurn: '', 
   score: '',
+  counter: 0,
+  indexArrayX: [],
+  indexArrayO: [],
 
-  test: function() {
-    console.log('this log works');
-  },
-
-  playerTurn: function() {
-
-  },
-
-  //x and o to loop through array until full or until winner is determined
 
   setPlay: function() {
-    
-
+  
   },
 
- checkWin: function() {
-    //if statement??
 
-    //accross
-    indexOf([0, 1, 2])
-    indexOf([3, 4, 5])
-    indexOf([6, 7, 8])
+  checkWin: function() {
 
-    //down
-    indexOf([0, 3, 6])
-    indexOf([1, 4, 7])
-    indexOf([2, 5, 8])
+    var sortArrayX = indexArrayX.sort()
+    var sortArrayO = indexArrayO.sort()
+    
+    var winningCombos = {
+                            //across
+                            [0, 1, 2],
+                            [3, 4, 5],
+                            [6, 7, 8],
+                            //down
+                            [0, 3, 6],
+                            [1, 4, 7],
+                            [2, 5, 8],
+                            //diagonal
+                            [0, 4, 8],
+                            [2, 4, 6]
+    };
 
-    //diagonal
-    indexOf([0, 4, 8])
-    indexOf([2, 4, 6])
+
+    // if === true {
+    //   console.log('x is the winner')
+    // }
+
   },
 //find underscore code to determine where x's or o's are.
 //if each index group equals either x or o, winner is determined; 
@@ -63,16 +63,29 @@ window.onload = function() {
 
     var index = parseInt(this.id.split('-')[1]); // getting the index number of the div being clicked to apply it back to the array
     console.log('index', index);
-    game.puzzle[index] = 'x'; // updating puzzle
+    // game.puzzle[index] = 'x';
 
-    for (var i=0; i < game.puzzle.length; i++) {
-      if (index % 2 === 0) {
-      $(this).html('x');
-      } else if (index % 2 === 1) {
-      $(this).html('o');
+    //COUNTER - alternate turns between x and o
+    var markerX = 'x';
+    var markerO = 'o';
+    game.counter++
+ 
+    // console.log(game.counter)
+    //each time div is clicked, counter++
+
+//MARKING X IN ARRAY, MARKING X ON THE BOARD, AND ALSO INPUTTING INDEX INTO NEW ARRAY FOR COMPARISON WITH WINNER
+      if (game.counter % 2 === 1) {
+        $(this).html(markerX);
+        game.puzzle[index] = markerX;
+        game.indexArrayX.push(index);
+        console.log('x appears at: ' + index);
+      } else {
+        $(this).html(markerO);
+        game.puzzle[index] = markerO;
+        game.indexArrayO.push(index);
+        console.log('o appears at: ' + index);
       }
-    }
-});
+  });
 
 
 }
